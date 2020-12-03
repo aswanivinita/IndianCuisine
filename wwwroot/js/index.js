90393a02af8f1f60d8037ea30ce9ac01956be9b3
aswanivinita <55902733+aswanivinita@users.noreply.github.com>
2020-12-03 14:02:43 -0800

    Add project files.

---
+++ wwwroot/js/index.js
@@ -0,0 +1,7 @@
+﻿var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    console.log("Buying Item");
+});
